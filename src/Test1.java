533ac142b4feba8af697bd137662c3385f0c2c1e
sakano1 <sakano1@ldsbc.edu>
2017-04-02 19:48:04 -0600

    Test 1 for push, commit and pull.

---
+++ src/Test1.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Scott on 4/2/2017.
+ *
+ * Test 1
+ */
+public class Test1 {
+    public static void main(String[] args){
+        System.out.println("This is a Test");
+    }
+}
